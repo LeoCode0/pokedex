6fa07701714144e635615602d59996a308a8a970
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-06-27 16:52:08 -0500

    Agregando descripcion de pokemon

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const api = 'https://pokeapi.co/api/v2/pokemon/';
+const api = 'https://pokeapi.co/api/v2';
 
 const central = document.getElementById('central')
 
@@ -7,6 +7,10 @@ const pokemonCard = async ({ url }) => {
     const pokemonId = data.id
     const pokemonName = data.name
     const pokemonImage = data.sprites.front_default
+    const height = (data.height / 10)
+    const weight = (data.weight / 10)
+    const pokemonSpecies = await getData(`${api}/pokemon-species/${pokemonId}/`)
+    const description = pokemonSpecies.flavor_text_entries[0].flavor_text
     let firstType = [ data.types.map(id => id.type.name)][0][0]
     let secondType = [ data.types.map(id => id.type.name)][0][1]
     secondType = checkUndefined(secondType)
@@ -29,9 +33,18 @@ const pokemonCard = async ({ url }) => {
         </div>
     </div>
     <div class="pokemonCard__skills">
-        <h1 class="">
-            Test
+        <h1 class="pokemonCard__skills--title">
+            Pokemon Information
         </h1>
+        <p class="pokemonCard__skills--description">
+            ${description.toLocaleLowerCase('en-US')}
+        </p>
+        <span class="pokemonCard__skills--height">
+            ${height} Meters
+        </span>
+        <span class="pokemonCard__skills--weight">
+            ${weight} Kilograms
+        </span>
     </div>
     `
     return template
@@ -54,10 +67,10 @@ const getData = async (API) => {
 };
 
 const printPokemon = async () => {
-    let base = await getData(api)
+    let base = await getData(`${api}/pokemon`)
     let results = await base.results
     let nextPage = await base.next
-    for(let index = 0; index <= 150; index++){
+    for(let index = 0; index <= 10; index++){
         if (index %20 === 0){
             for(let i = 0; i <= 19; i++){
                 const container = document.createElement('div')
@@ -78,7 +91,31 @@ const printPokemon = async () => {
     }
 }
 
+
+// const test = async () => {
+//     let base = await getData(api)
+//     let results = await base.results
+//     let nextPage = await base.next
+//     let r = await getData(nextPage)
+//     let counter = 0
+
+//     console.log(results.map(x => {
+//         counter += 1
+//         if (counter === 20 ){
+//             console.log('Test')
+//             return x.name
+//         } else{
+//             return x.name
+//         }
+//     }))
+// }
+
+// test()
+
+
 printPokemon()
+
+
 
 // const main =  async () => {
 //     const data = await getData(api);
